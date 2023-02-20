93cc9d74de01a5ca540d1981702b76ef0d08962c
Harsh <imharshalashi333@gmail.com>
2023-02-20 19:28:01 +0530

    use of localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RecipeList from './components/recipe/RecipeList';
 import './components/css/app.css';
 import { v4 as uuidv4 } from 'uuid';
 
 export const RecipeContext = React.createContext();
 
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes';
+
 const App = () => {
-  const [recipes, setRecipes] = useState(sampleRecipes);
+  const [recipes, setRecipes] = useState(() => {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || []);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
+  }, [recipes]);
+
+  //Solution for without React.StrictMode
+
+  // const [recipes, setRecipes] = useState(sampleRecipes);
+
+  // useEffect(() => {
+  //   const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+  //   if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON));
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
+  // }, [recipes]);
 
   const handleAddRecipe = () => {
     const newRecipe = {
